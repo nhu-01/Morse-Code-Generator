fd9f0fd3a7df49cefcf4cfd4399d2fbf5ce9a61a
nhu <156723883+nhu-01@users.noreply.github.com>
2025-11-17 22:11:22 -0800

    Update morse_tree.h

--- morse_tree.h
+++ morse_tree.h
@@ -9,28 +9,22 @@ template<typename Type>
 class MorseTree
 {
 private:
-    // place your code here
     Node<Type>* root;
-    
-    //helper functions
+
     void insert(Node<Type>* node, const string& code, Type letter, size_t index);
     void clear(Node<Type>* node);
     bool encodeLetter(Node<Type>* node, Type letter, string code, string& result) const;
     Type decodeLetter(const string& code) const;
 
-
 public:
-    // constructor
-    MorseTree(); // initialize root
-    MorseTree(const string& filename); //load Morse code file
-    ~MorseTree(); //destructor
+    MorseTree();
+    MorseTree(const string& filename);
+    ~MorseTree();
 
-    //methods
     void insert(const string& code, Type letter);
     void clear();
     string encodeText(const string& text) const;
     string decodeText(const string& code) const;
-
 };
 
 #include "morse_tree.cpp"
